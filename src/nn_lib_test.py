15da3a58e5b46b02e5e1d38b660760f9498b95d1
gt <gt@tajnafoi.name>
2019-02-19 21:57:42 +0100

    test

---
+++ src/nn_lib_test.py
@@ -0,0 +1,58 @@
+import unittest
+import numpy as np
+from nn_lib import *
+
+class TestLinearLayer(unittest.TestCase):
+
+    def test_as_recomended_sizes(self):
+        batch_size = 1
+        n_in = 3
+        n_out = 42
+        learning_rate = 0.01
+        layer = LinearLayer(n_in=n_in, n_out=n_out)
+        inputs = np.random.rand(batch_size, n_in)
+        outputs = layer(inputs)
+        self.assertEqual(outputs.shape[0], batch_size)
+        self.assertEqual(outputs.shape[1], n_out)
+
+        grad_loss_wrt_outputs = np.random.rand(batch_size, n_out)
+        grad_loss_wrt_inputs = layer.backward(grad_loss_wrt_outputs)
+        self.assertEqual(grad_loss_wrt_inputs.shape[0], batch_size)
+        self.assertEqual(grad_loss_wrt_inputs.shape[1], n_in)
+        layer.update_params(learning_rate)
+        pass
+
+    def test_1x1(self):
+        batch_size = 1
+        n_in = 1
+        n_out = 1
+        learning_rate = 0.01
+        layer = LinearLayer(n_in=n_in, n_out=n_out)
+        layer._W = 2
+        layer._b = 3
+        inputs = [[1.0]]
+        outputs = layer(inputs)
+        self.assertEqual(outputs.shape[0], batch_size)
+        self.assertEqual(outputs.shape[1], n_out)
+        self.assertEqual(outputs, [[5.0]])  # 2*1 + 3
+
+        grad_loss_wrt_outputs = np.zeros((batch_size, n_out))
+        grad_loss_wrt_inputs = layer.backward(grad_loss_wrt_outputs)
+        self.assertEqual(grad_loss_wrt_inputs.shape[0], batch_size)
+        self.assertEqual(grad_loss_wrt_inputs.shape[1], n_in)
+        layer.update_params(learning_rate)
+        pass
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper() or True)
+    #
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
