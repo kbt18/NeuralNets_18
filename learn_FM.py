a4140427775aa100f3fe2df51770e52b53b07151
kbt18 <kbt18@ic.ac.uk>
2019-02-21 19:00:43 +0000

    finished 2.1

--- learn_FM.py
+++ learn_FM.py
@@ -1,9 +1,13 @@
+import keras
 from keras.layers import Dense
 from keras.models import Sequential
 import matplotlib.pyplot as plt
 from keras.callbacks import EarlyStopping
+from keras.regularizers import l2
+from keras.layers import Dropout
 
 import numpy as np
+
 
 from nn_lib import (
     MultiLayerNetwork,
@@ -14,12 +18,9 @@ from nn_lib import (
 )
 from illustrate import illustrate_results_FM
 
-def train_model(x, y):
 
-
-
-    history = model.fit(x, y, batch_size=64, epochs=1000, validation_split=0.2, callbacks=[early_stopper])
-
+def evaluate_architecture(model, valdation_set):
+    return None
 
 def main():
     dataset = np.loadtxt("FM_dataset.dat")
@@ -27,20 +28,39 @@ def main():
     #                       ** START OF YOUR CODE **
     #######################################################################
 
+    ############################ Question 1 ###############################
     model = Sequential([
-        Dense(32, activation='tanh', input_shape=(3,)),
-        Dense(32, activation='tanh', input_shape=(3,)),
-        Dense(32, activation='tanh', input_shape=(3,)),
-        Dense(3, activation='linear'),
+        Dense(8, activation='tanh', input_shape=(3,)),
+        Dense(64, activation='tanh'),
+        Dense(64, activation='tanh'),
+        Dense(64, activation='tanh'),
+        Dense(3, activation='linear')
     ])
 
+    keras.optimizers.Adam(lr=0.01)
     model.compile(loss="mse", optimizer="adam", metrics=['mae'])
-    early_stopper = EarlyStopping(patience=20, verbose=1, restore_best_weights=False)
+    early_stopper = EarlyStopping(patience=20, verbose=1, restore_best_weights=True)
+
+    np.random.shuffle(dataset)
+    x, y = dataset[:, :3], dataset[:, 3:]
+
+    split_idx = int(0.8 * len(x))
+
+    x_train = x[:split_idx]
+    y_train = y[:split_idx]
+    x_val = x[split_idx:]
+    y_val = y[split_idx:]
+
+    history = model.fit(x_train, y_train, batch_size=8, epochs=200, callbacks=[early_stopper])
+    print(model.evaluate(x_val, y_val))
+
+    ############################ Question 2/3 ###############################
+
 
     #######################################################################
     #                       ** END OF YOUR CODE **
     #######################################################################
-    illustrate_results_FM(network, prep)
+    #illustrate_results_FM(network, prep)
 
 
 if __name__ == "__main__":
