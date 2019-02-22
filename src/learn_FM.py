0462df4d9110775f5ce9bdb6c2b765478406b6d4
gt4518 <gabor.tajnafoi18@imperial.ac.uk>
2019-02-22 19:39:08 +0000

    grid search - initial

--- src/learn_FM.py
+++ src/learn_FM.py
@@ -1,26 +1,117 @@
+from keras.layers import Dense
+from keras.models import Sequential
+import matplotlib.pyplot as plt
 import numpy as np
+from keras.callbacks import EarlyStopping
 
-from nn_lib import (
-    MultiLayerNetwork,
-    Trainer,
-    Preprocessor,
-    save_network,
-    load_network,
-)
-from illustrate import illustrate_results_FM
 
+# evaluate architecture run lots of times for different paramters and the paramter_split might split the data differently each time
+# also we want to return how well the model is performing
+
+def evaluate_architecture(x_train, y_train, x_val, y_val, layers=[32, 32, 32],
+                          activation_func='tanh', optimizer='adam', loss='mse'):
+    model = Sequential()
+    # adding the first layer should be outside of the for loop because it needs to take the input_shape, unlike the other layers
+    model.add(Dense(layers[0], activation=activation_func, input_shape=(3,)))
+    for neurons in layers[1:]:
+        model.add(Dense(neurons, activation=activation_func))
+    model.add(Dense(3, activation='linear'))
+    model.compile(optimizer=optimizer, loss=loss, metrics=['mae', 'mse'])
+    early_stopper = EarlyStopping(patience=20, verbose=1, restore_best_weights=False)
+    history = model.fit(x_train, y_train, batch_size=64, epochs=1000,
+                        callbacks=[early_stopper], verbose=1, validation_data=(x_val, y_val))
+    # could plot these graphs here, which shows how the loss and validation loss decreases over the epochs
+    # epochs is on the x axis and the loss and val_loss are on the y axis
+    #     plt.plot(history.history['loss'])
+    #     plt.plot(history.history['val_loss'])
+    return model.evaluate(x_val, y_val, batch_size=256), model
+
+def train_model(x, y):
+    model = Sequential([
+        Dense(32, activation='tanh', input_shape=(3,)),
+        Dense(32, activation='tanh', input_shape=(3,)),
+        Dense(32, activation='tanh', input_shape=(3,)),
+        # output later ahas a linear activiation function because relu and sigmoid don't allow more than 1 or less than 0
+        Dense(3, activation='linear'),
+    ])
+
+    # model.summary()
+
+    model.compile(loss="mse", optimizer="adam", metrics=['mae'])
+
+    # train the model, batch size is how many examples we look at before updating the weights,
+    # epochs is how often we want to iterate over our whole dataset
+    # EarlyStopping stops the training if the validation loss doesn't improve anymore so it doesn't actually get to 1000
+    # patience specificies how many epochs we wait to get better validaiton loss before we stop training
+    early_stopper = EarlyStopping(patience=20, verbose=1, restore_best_weights=False)
+    history = model.fit(x, y, batch_size=64, epochs=1000, validation_split=0.2, callbacks=[early_stopper])
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['val_loss'])
+    #
+    # plt.plot(history.history['mean_absolute_error'])
+    # plt.plot(history.history['val_mean_absolute_error'])
+    # make predictions based on the input data, we are just using our data x here but we should be using the data they give us
+    return model
 
 def main():
     dataset = np.loadtxt("FM_dataset.dat")
-    #######################################################################
-    #                       ** START OF YOUR CODE **
-    #######################################################################
 
-    #######################################################################
-    #                       ** END OF YOUR CODE **
-    #######################################################################
-    illustrate_results_FM(network, prep)
+     # Question 1
 
+    np.random.shuffle(dataset)
+    x, y = dataset[:, :3], dataset[:, 3:]
+    model = train_model(x, y)
+    model.save("./fm_model.dat")
+    # make prediction using model using input z
+    # model.predict(z)
+
+    # illustrate_results_FM(network, prep)
+
+
+    # Question 2
+
+    # Need to split split training dataset and validation set manually because the keras
+    # shuffle only shuffles after splitting off the group in the validation set
+
+    # nb_train_examples = int(len(x) * 0.8)
+    # x_train, y_train = x[:nb_train_examples], y[:nb_train_examples]
+    # x_val, y_val = x[nb_train_examples:], y[nb_train_examples:]
+    #
+    # # evaluate_architecture returns them model so we can save it and compare with others
+    # # to find best parameters
+    # metrics, model_e = evaluate_architecture(x_train, y_train, x_val, y_val,
+    #                                        layers=[32, 16], activation_func='tanh',
+    #                                        loss='mae')
+    # print(metrics)
 
 if __name__ == "__main__":
     main()
+
+
+
+# import numpy as np
+#
+# from nn_lib import (
+#     MultiLayerNetwork,
+#     Trainer,
+#     Preprocessor,
+#     save_network,
+#     load_network,
+# )
+# from illustrate import illustrate_results_FM
+#
+#
+# def main():
+#     dataset = np.loadtxt("FM_dataset.dat")
+#     #######################################################################
+#     #                       ** START OF YOUR CODE **
+#     #######################################################################
+#
+#     #######################################################################
+#     #                       ** END OF YOUR CODE **
+#     #######################################################################
+#     illustrate_results_FM(network, prep)
+#
+#
+# if __name__ == "__main__":
+#     main()
--- src/learn_ROI.py
+++ src/learn_ROI.py
@@ -1,4 +1,11 @@
 import numpy as np
+from keras.layers import Dense
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+from keras.callbacks import EarlyStopping
+#from sklearn.model_selection import GridSearchCV
+
+
 
 from nn_lib import (
     MultiLayerNetwork,
@@ -10,17 +17,102 @@ from nn_lib import (
 
 from illustrate import illustrate_results_ROI
 
+def network_test(np_array):
+    assert(np_array.shape[1] == 3)
+    return np.random.rand(np_array.shape[0], 4)
+
+# apply min max scaling norm
+class Preproc:
+    def __init__(self, x):
+        pass
+    def apply(self, x):
+        return x
+
+    def revert(self, x):
+        return x
+
+# create an architecture for the model
+def model_params(data, x, y, num_hidden, num_neurons_inlayer, activation, final_activation):
+    model = Sequential([
+        Dense(num_neurons_inlayer, activation=activation, input_shape=(3,)), # 3 input angles
+    ])
+
+    # add hidden layers
+    for i in range(num_hidden -1):
+        model.add(Dense(num_neurons_inlayer, activation=activation))
+    # # add last layer
+    model.add(Dense(4, activation=final_activation)) # output is 4 because there are 4 regions
+
+    #train model
+    model.compile(loss="mse", optimizer="adam", metrics=['mae'])
+    early_stopper = EarlyStopping(patience=20, verbose=1, restore_best_weights=False)
+    history = model.fit(x, y, batch_size=data.shape[0], epochs=1000, validation_split=0.2, callbacks=[early_stopper], verbose=0)
+    return model
+
+def train_model(dataset, prep):
+    splitindex = int(dataset.shape[0] * 0.2)
+    test = dataset[:splitindex, :]
+    train_dataset = dataset[splitindex:,:]
+    # k-fold split - from cw1
+    x = train_dataset[:,0:3]
+    y = train_dataset[:,3:7]
+    x_val = test[:,0:3]
+    y_val = test[:,3:7]
+
+    data = prep.apply(dataset)
+    results = []
+    # iterate through different architectures
+
+    # neurons = 1
+    # activations = "selu"
+    # hiddenlayers = 1
+    # model = model_params(data, x, y, hiddenlayers, neurons, activations, final_activation)
+    # model.summary()
+    # eval_result = model.evaluate(x_val, y_val) # return [loss, metrics]
+    # results.append((eval_result, activations, hiddenlayers, neurons))
+
+    for final_activation in ["linear","softmax"]:
+        for activations in ["tanh", "relu", "sigmoid", "hard_sigmoid", "selu"]:
+            for hiddenlayers in range(1,12):
+                for neurons in range(1, 10):
+                    model=model_params(data, x, y, hiddenlayers, neurons, activations, final_activation)
+                    eval_result = model.evaluate(x_val, y_val)
+                    results.append((eval_result, activations, hiddenlayers, neurons, final_activation))
+
+    for tuple in results:
+        # i+=1
+        print(tuple)
+        # print(results[i])
+
+    model.save("./roi_model.dat")
+    return model
 
 def main():
     dataset = np.loadtxt("ROI_dataset.dat")
     #######################################################################
     #                       ** START OF YOUR CODE **
+    # aim: predict which zone the pointer arm is based on the angular position of the arm segments
+    # arm segment angles: columns 0,1,2, one hot encoded labels: 3,4,5,6
     #######################################################################
+
+    prep = Preproc(dataset)
+    # mlnet = MultiLayerNetwork(prep)
+    # save_network(network)
+
+    model = train_model(dataset, prep)
+
+    def network(three_angle):
+        return model.predict(three_angle)
+    # np.random.shuffle(dataset)
+    # x, y = dataset[:, :3], dataset[:, 3:]
+    # model = train_model(x, y)
+    # model.save("./fm_model.dat")
 
     #######################################################################
     #                       ** END OF YOUR CODE **
     #######################################################################
-    illustrate_results_ROI(network, prep)
+    # graph model - 10 random locations - prediction vs reality check
+    # illustrate_results_ROI(network, prep)
 
 
 if __name__ == "__main__":
