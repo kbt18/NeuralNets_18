f31ddbb6b364db3f3ccd31318f11dec49a44a617
kbt18 <kbt18@ic.ac.uk>
2019-02-25 17:54:32 +0000

    changed patience to 10

--- learn_FM.py
+++ learn_FM.py
@@ -52,7 +52,7 @@ def main():
 
     keras.optimizers.Adam(lr=0.001)
     model.compile(loss="mse", optimizer="adam", metrics=['mae'])
-    early_stopper = EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True)
+    early_stopper = EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)
 
     np.random.shuffle(dataset)
     x, y = dataset[:, :3], dataset[:, 3:]
@@ -66,7 +66,7 @@ def main():
 
     history = model.fit(x, y, validation_split=0.2, batch_size=32, epochs=300, callbacks=[early_stopper])
     #history = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=100, callbacks=[early_stopper])
-    print(model.evaluate(x_val, y_val))
+    #print(model.evaluate(x_val, y_val))
 
     ############################ Question 2/3 ###############################
 
